f2266e2cc5807c899a2efd8fdf79a041738e67d8
danielmills <danielmillst@gmail.com>
2016-03-21 22:10:50 -0400

    Color utilities

---
+++ com/glacialrush/motd/ColorUtils.java
@@ -0,0 +1,31 @@
+package com.glacialrush.motd;
+
+import java.util.List;
+
+public class ColorUtils
+{
+	private static boolean bungee = false;
+	
+	public static List<String> color(List<String> strings)
+	{
+		if(bungee)
+		{
+			return CUBungee.color(strings);
+		}
+		return CUBukkit.color(strings);
+	}
+	
+	public static String strip(String s)
+	{
+		if(bungee)
+		{
+			return CUBungee.strip(s);
+		}
+		return CUBukkit.strip(s);
+	}
+	
+	public static void setBungee()
+	{
+		bungee = true;
+	}
+}
